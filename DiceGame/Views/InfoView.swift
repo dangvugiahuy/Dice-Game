f1a0514bb34564b3acac6edb9074f0919e5ed239
GiaHuy <dangvugiahuy2012002@gmail.com>
2024-04-13 14:17:54 +0700

    (Update) remove section of school information

--- DiceGame/Views/InfoView.swift
+++ DiceGame/Views/InfoView.swift
@@ -35,28 +35,6 @@ struct InfoView: View {
                     Text("- If you lose, you will be deducted x5 of the bet amount")
                     Text("You can reset the money and highscore by clicking on the button Reset.")
                 }
-                Section(header: Text("Application Information")) {
-                    HStack {
-                      Text("App Name")
-                      Spacer()
-                      Text("RMIT Dice Game")
-                    }
-                    HStack {
-                      Text("Course")
-                      Spacer()
-                      Text("COSC2659")
-                    }
-                    HStack {
-                      Text("Year Published")
-                      Spacer()
-                      Text("2022")
-                    }
-                    HStack {
-                      Text("Location")
-                      Spacer()
-                      Text("Saigon South Campus")
-                    }
-                }
             }
             .font(.system(.body, design: .rounded))
           }
